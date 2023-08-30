f2fed262ea7135742645318b3ae702de3a603e75
Danil Boldyrev <daswerq123@gmail.com>
2023-08-30 21:26:41 +0300

    fix

--- split_audio.bat
+++ split_audio.bat
@@ -1,10 +1,9 @@
 @echo off
 
 :: change path for your
-set "AUDIO_PATH=audio.mp3"
-set "OUTPUT_FOLDER=output"
-set "DELAY_MS=300"
-
+set "AUDIO_PATH=out.wav"
+set "OUTPUT_FOLDER=spongeBob"
 
 call venv/scripts/activate
-python .\split.py %AUDIO_PATH%" %OUTPUT_FOLDER% --pause %DELAY_MS%
+python .\split.py "%AUDIO_PATH%" "%OUTPUT_FOLDER%"
+pause
