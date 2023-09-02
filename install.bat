1baea129f7fab348c10e762fcdce8f23b11c220a
Danil Boldyrev <daswerq123@gmail.com>
2023-09-03 01:39:01 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,6 +2,6 @@ python -m venv venv
 call venv/scripts/activate
 
 
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install git+https://github.com/m-bain/whisperx.git
 pip install pydub
